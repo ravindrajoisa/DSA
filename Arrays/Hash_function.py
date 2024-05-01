88be5ee73d6ad0de3c9f7cac4c9344aeafe85a0e
ravindrajoisa <ravindra.joisa@hotmail.com>
2024-05-01 11:37:09 +0530

    hash function

--- Arrays/Hash_function.py
+++ Arrays/Hash_function.py
@@ -6,3 +6,6 @@ def hash_function(value):
     return sum_of_chars % 10
 
 print("'Ravi' has a hash code: ", hash_function('Ravi'))
+
+# Before: my_hash_set = [None,None,None,None,None,'Bob',None,None,None,None]
+# After: my_hash_set = [None,None,Ravi,None,None,'Bob',None,None,None,None]
